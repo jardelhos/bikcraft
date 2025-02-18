a8842ef74d29cb41221dec4af51f50f6aecbd0b9
Jardel Oliveira <jardel-h@hotmail.com>
2025-02-18 16:08:57 -0300

    parametros URL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//ativar links do menu//
+
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link){
@@ -10,3 +12,17 @@ function ativarLink(link){
 
 
 links.forEach(ativarLink)
+
+//ativar links do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro){
+    const elemento = document.getElementById(parametro);
+    if(elemento){
+        elemento.checked = true;
+    }
+    console.log(parametro);
+}
+
+parametros.forEach(ativarProduto);
